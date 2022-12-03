cf2a38cb511816c3bc46ea80df452ace302d31e1
msaleh <mahmoud.saleh@edafa.com>
2022-12-03 21:17:26 +0200

    ad reverseproxy puplic URL

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a8d0803badc96477b91ae4ab9fe6454d-155107905.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
